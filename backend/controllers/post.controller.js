ed81f29754eec295eedf3ce09bba045af9814c64
Vinicius França <114887277+viniciusfranfer@users.noreply.github.com>
2025-02-07 23:43:53 -0300

    update post controller functions

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,7 +1,106 @@
+import Notification from '../models/notification.model.js';
 import Post from '../models/post.model.js';
 import User from '../models/user.model.js';
 
 import { v2 as cloudinary } from 'cloudinary';
+
+export const getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ createdAt: -1 }).populate({
+      path: "user",
+      select: "-password",
+    })
+      .populate({
+        path: "comments.user",
+        select: "-password",
+      })
+
+    if (posts.length === 0) return res.status(404).json([]);
+
+    res.status(200).json(posts);
+
+  } catch (error) {
+    console.log(`Error in getAllPosts controller: ${error.message}`);
+    res.status(500).json({ error: error.message });
+  }
+}
+
+export const getFollowingPosts = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const user = await User.findById(userId);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const following = user.following;
+
+    const feedPosts = await Post.find({ user: { $in: following } })
+      .sort({ createdAt: -1 })
+      .populate({
+        path: "user",
+        select: "-password",
+      })
+      .populate({
+        path: "comments.user",
+        select: "-password",
+      })
+
+    res.status(200).json(feedPosts);
+
+  } catch (error) {
+    console.log(`Error in getFollowingPosts controller: ${error.message}`);
+    res.status(500).json({ error: error.message });
+  }
+}
+
+export const getLikedPosts = async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const user = await User.findById(userId);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const likedPosts = await Post.find({ _id: { $in: user.likedPosts } })
+      .populate({
+        path: "user",
+        select: "-password",
+      }).populate({
+        path: "comments.user",
+        select: "-password",
+      })
+
+    res.status(200).json(likedPosts);
+
+  } catch (error) {
+    console.log(`Error in getLikedPosts controller: ${error.message}`);
+    res.status(500).json({ error: error.message });
+  }
+}
+
+export const getUserPosts = async (req, res) => {
+  try {
+    const { username } = req.params;
+
+    const user = await User.findOne({ username });
+    if (!user) return res.status(404).json({ error: "User not found" });
+
+    const posts = await Post.find({ user: user._id })
+      .sort({ createdAt: -1 })
+      .populate({
+        path: "user",
+        select: "-password",
+      })
+      .populate({
+        path: "comments.user",
+        select: "-password",
+      })
+
+      res.status(200).json(posts);
+
+  } catch (error) {
+    console.log(`Error in getUserPosts controller: ${error.message}`);
+    res.status(500).json({ error: error.message });
+  }
+}
 
 export const createPost = async (req, res) => {
   try {
@@ -34,9 +133,73 @@ export const createPost = async (req, res) => {
   }
 };
 
-export const likeUnlikePost = async (req, res) => { }
+export const likeUnlikePost = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const { id } = req.params;
 
-export const commentOnPost = async (req, res) => { }
+    const post = await Post.findById(id);
+    if (!post) return res.status(404).json({ message: "Post not found" });
+
+    const userLikedPost = post.likes.includes(userId);
+
+    let notification = new Notification({
+      from: userId,
+      to: post.user,
+      type: "like",
+    });
+
+    if (userLikedPost) {
+      await Post.updateOne({ _id: id }, { $pull: { likes: userId } });
+      await User.updateOne({ _id: userId }, { $pull: { likedPosts: id } });
+      res.status(200).json({ message: "Post unliked successfully" });
+
+      notification = await Notification.findOneAndDelete({ from: userId, to: post.user, type: "like" });
+    } else {
+      post.likes.push(userId);
+      await User.updateOne({ _id: userId }, { $push: { likedPosts: id } });
+      await post.save();
+
+      await notification.save();
+
+      res.status(200).json({ message: "Post liked successfully" });
+    }
+
+  } catch (error) {
+    console.log(`Error in likeUnlikePost controller: ${error.message}`);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const commentOnPost = async (req, res) => {
+  try {
+    const { text } = req.body;
+    let { img } = req.body;
+    const userId = req.user._id;
+    const postId = req.params.id;
+
+    if (!text && !img) return res.status(400).json({ error: "Text or image is required for a Comment" });
+
+    if (img) {
+      const updatedResponse = await cloudinary.uploader.upload(img);
+      img = updatedResponse.secure_url;
+    }
+
+    const post = await Post.findById(postId);
+    if (!post) return res.status(404).json({ message: "Post not found" });
+
+    const comment = { user: userId, text, img };
+
+    post.comments.push(comment);
+    await post.save();
+
+    res.status(200).json(post);
+
+  } catch (error) {
+    console.log(`Error in commentOnPost controller: ${error.message}`);
+    res.status(500).json({ error: error.message });
+  }
+};
 
 export const deletePost = async (req, res) => {
   try {

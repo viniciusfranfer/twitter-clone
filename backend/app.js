28866b43dbd664939e8bf4db016dc3375fab7d31
Vinicius França <114887277+viniciusfranfer@users.noreply.github.com>
2025-02-10 15:29:13 -0300

    create notification routes and controller

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ import connectMongoDB from "./database/connectMongoDB.js";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
+import notificationRoutes from "./routes/notification.route.js";
 
 dotenv.config();
 
@@ -29,6 +30,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

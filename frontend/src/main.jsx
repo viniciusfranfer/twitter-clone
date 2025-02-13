9e6939b8749672a746567d489c652b6816d23a3f
Vinicius França <114887277+viniciusfranfer@users.noreply.github.com>
2025-02-12 22:47:41 -0300

    update query default options

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,7 +8,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      refetchOnWindowFocus: false
+      refetchOnWindowFocus: true
     },
   },
 });
